fecd5ccc0e78077abace96c9ffd13c184baf6b6d
soragoto <soragoto@soragoto.io>
2024-11-01 18:37:05 +0800

    新建用户手动设置用户名

--- src/views/players/PlayersView.vue
+++ src/views/players/PlayersView.vue
@@ -1,33 +1,51 @@
 <template>
     <UserCell />
     <t-fab :icon="iconFunc" @click="onClick" :style="fabStyle" />
+
+    <t-dialog v-model:visible="visible" title="设置玩家名称" confirm-btn="确定" cancel-btn="取消" @confirm="onAddPlayerConfirm()">
+        <t-input v-model:value="newName" borderless class="dialog-input" clearable slot="content" placeholder="输入名称"
+            placeholder-class="placeholder" />
+    </t-dialog>
+
 </template>
 
 <script setup lang="ts">
 
-import { useGameStore, saveConfig } from '@/stores/storage';
+import { saveConfig, useGameStore } from '@/stores/storage';
 import type { Player } from '@/ts/common';
-import { genNonDuplicateID } from '@/ts/common';
 import { AddIcon } from 'tdesign-icons-vue-next';
-import { h } from 'vue';
+import { h, ref } from 'vue';
 import UserCell from '../../components/UserCell.vue';
+import { Toast } from 'tdesign-mobile-vue';
 
 const gameStore = useGameStore()
 
-const iconFunc = () =>  h(AddIcon, { size: '30px' })
+const iconFunc = () => h(AddIcon, { size: '30px' })
 const fabStyle = "bottom: 80px; right: 5dvw"
 
-const onClick = () => {
+const visible = ref(false)
+const newName = ref('')
+
+const onAddPlayerConfirm = () => {
+
+    if (gameStore.playerListMap.has(newName.value)) {
+        Toast.error('用户已存在！')
+        return
+    }
+
     var player: Player = {
-        name: '牌搭子' + genNonDuplicateID(),
+        name: newName.value,
         seat: '',
         point: gameStore.startPoint,
         riichi: false
     }
     gameStore.setPlayer(player.name, player)
-
     saveConfig()
-};
+    newName.value = ''
+}
 
+const onClick = () => {
+    visible.value = true
+}
 
 </script>
